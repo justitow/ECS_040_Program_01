9b4a42cc64d13fe20ab7c1841e4b51d43e41b55c
justitow <justitow@gmail.com>
2016-01-22 14:22:24 -0800

    Justin commit

--- decoder.c
+++ decoder.c
@@ -24,7 +24,7 @@ void andl(int *op1, int *op2)
 void leave(Registers *registers, int memory[])
 {
     registers->regs[esp] = registers->regs[ebp];
-    registers->regs[esp] = memory[registers->regs[ebp]]+4;
+    registers->regs[esp] = memory[registers->regs[ebp]] + 4;
 } //leave ()
 
 
@@ -56,35 +56,44 @@ void subl(int *op1, int *op2)
 
 void parse_operand(Registers *registers, Decoder* decoder, int memory[])
 {
-    if(!(strcmp(decoder->opcode, "addl"))){
+    
+    if(!(strcmp(decoder->opcode, "addl")))
+    {
         addl(decoder->operand1, decoder->operand2);
     }
     
-    else if (!(strcmp(decoder->opcode, "andl"))){
+    else if (!(strcmp(decoder->opcode, "andl")))
+    {
         andl(decoder->operand1, decoder->operand2);
     }
     
-    else if(!(strcmp(decoder->opcode, "leave"))){
+    else if(!(strcmp(decoder->opcode, "leave")))
+    {
         leave(registers, memory);
     }
     
-    else if(!(strcmp(decoder->opcode, "movl"))){
+    else if(!(strcmp(decoder->opcode, "movl")))
+    {
         movl(decoder->operand1, decoder->operand2);
     }
     
-    else if(!(strcmp(decoder->opcode, "pushl"))){
+    else if(!(strcmp(decoder->opcode, "pushl")))
+    {
         pushl(decoder->operand1, memory, registers);
     }
     
-    else if(!(strcmp(decoder->opcode, "ret"))){
+    else if(!(strcmp(decoder->opcode, "ret")))
+    {
         ret(registers, memory);
     }
     
-    else if(!(strcmp(decoder->opcode, "subl"))){
+    else if(!(strcmp(decoder->opcode, "subl")))
+    {
         subl(decoder->operand1, decoder->operand2);
     }
     
-    else{
+    else
+    {
         fprintf(stderr, "An error occured: %s\n", decoder->opcode);
     }
 } //parse_operand ()
@@ -100,14 +109,19 @@ void parse_instruction(Registers* registers, Decoder* decoder, char* instruction
     strcpy(decoder->opcode, token);
     
     printf("%s  ", token);
-    while(token != NULL){
+    
+    while(token != NULL)
+    {
         token = strtok(NULL, " ,");
+        
         if(decoder->operand1 == NULL && token != NULL)
         {
             printf("%s", token);
             decoder->operand1 = address(registers, token, memory);
         }
-        else if(decoder->operand2 == NULL && token != NULL) {
+        
+        else if(decoder->operand2 == NULL && token != NULL)
+        {
             printf(", %s     ", token);
             decoder->operand2 = address(registers, token, memory);
         }
